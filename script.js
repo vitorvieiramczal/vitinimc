02c808fc9521c2093a1852c7e6f9c1c491ed92a9
vitorvieiramczal <139259045+vitorvieiramczal@users.noreply.github.com>
2024-02-19 18:25:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function peso(){
+    const valor1 = document.getElementById("valor1")
+    const valor2 = document.getElementById("valor2")
+
+    const numero1 = Number(valor1.value)
+    const numero2 = Number(valor2.value)
+
+    = peso / (numero1 * numero2)
+
+    document.getElementById("resultado").innerText = "resultado: " + peso
+}
+
+
+
+
+
+
+
+// atividade 1
+
+// const array1 = [21, 6, 84, 24, 86, 11, 39, 59, 96, 53];
+
+// const map1 = array1.map((x) => x * 2);
+
+// console.log(result);
+
+// atividade 2
+
+// const words = ["sol", "livro", "lua", "cadeira", "mesa", "mar"]
+
+// const result = words.filter((word) => word.length > 6);
+
+// console.log(result);
+
+// atividade 3 
+
